9a767205c2e46cf25cbc005fbfed975d3e234449
Yan Bonini Falcão <yan-work@hotmail.com>
2021-03-31 22:10:57 -0300

    ASPNet Core Migrations

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,22 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // String de Conexão
+        {
+            optionsBuilder.UseSqlServer("Password=paranoia13;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-T9A7732/SQLEXPRESS");
+        }
+
+    }
+}
